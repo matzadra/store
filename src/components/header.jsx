6860cba6a6f3bbb4a7c4711846527a0bf65e4581
Matheus Zadra <matheus_eu.zz@hotmail.com>
2021-11-24 04:51:09 -0300

    Adjustments

---
+++ src/components/header.jsx
@@ -0,0 +1,26 @@
+// class HelloWorld extendes React.Component {
+//     constructor(props){
+//     super(props);
+//     this.state ={
+//     products[{
+//     name : " ",
+//     price : " "
+//     ]}
+//     }
+    
+//     function = () => {
+//     let last_planet = this.state.planets[this.state.planets.length - 1]
+//     this.setState(state => ({
+//     planets:[...this.state.planets]
+//     }))
+//     }
+    
+//     incrementCount(){
+//     this.setState((state) => {return {count : state.count +1 }}
+//     }
+    
+//     render(){
+//     return <h1>
+//     }
+//     }
+    
